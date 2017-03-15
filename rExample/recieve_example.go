a14d2a3dacdcc8ef57a368a88d1f56dcb2dce14d
Pete <pafischer1992@gmail.com>
2017-03-15 17:28:08 +0100

    Refactor because of GoLang syntax stuff

--- rExample/recieve_example.go
+++ rExample/recieve_example.go
@@ -9,7 +9,7 @@ import (
 
 // RecieveHello shit
 func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:1337/")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
--- sExample/send_example.go
+++ sExample/send_example.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:1337/")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
