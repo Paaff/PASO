af37274a2ef8aa0db0e17376048987e3616eabbc
Pete <pafischer1992@gmail.com>
2017-03-24 16:08:33 +0100

    Started with bluetooth class discovery

--- client/client.go
+++ client/client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"fmt"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
@@ -40,6 +41,31 @@ func trimBtOutput(inq []byte) {
 		}
 	}
 
+}
+
+// Takes a hexadecimal number and interprets the binary representation as what class is embedded there.
+func discoverBtClass(hexClass string) bool {
+	// Strip the identifier 0x
+	rawHex := hexClass[2:]
+
+	// Convert string to int
+	classInt, err := strconv.ParseUint(rawHex, 16, 32)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	// Convert int to binary representation
+	// %024b indicates base 2, padding with 0, with 24 characters.
+	classBin := fmt.Sprintf("%024b", classInt)
+
+	// Find out if the binary representation matches that of a phone.
+	/*
+		Bit 22 = Telephony
+		Bit 12-11-10-9-8 = 00010 = Phone
+		Bit 7-6-5-4-3-2 = 000011 = Smart Phone
+	*/
+
+	return false
 }
 
 // Wifi detection
