920120f5d876cbf1aee2a1f1bbc6da9a28ced70a
Pete <pafischer1992@gmail.com>
2017-04-23 13:52:35 +0200

    Web board initializes when server starts.

--- server/server.go
+++ server/server.go
@@ -8,10 +8,13 @@ import (
 	"github.com/paaff/PASO/client"
 	"github.com/paaff/PASO/config"
 	"github.com/paaff/PASO/wabbit"
+	"github.com/paaff/PASO/web"
 )
 
 // Start - Global function to start the server.
 func Start(conf *config.Config) {
+	web.InitBoard()
+
 	// Start connection with RabitMQ server.
 	w, err := wabbit.InitWabbitConsumer(conf.Username, conf.Pass, conf.Address, conf.Port, "bluetoothqueue", conf.ExchangeName, conf.ExchangeType, conf.RoutingKey)
 	if err != nil {
