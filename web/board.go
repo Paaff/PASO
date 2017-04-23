755f6f22c28e198748f19f9bc56d3ffcfeaa89dc
Pete <pafischer1992@gmail.com>
2017-04-23 14:03:09 +0200

    Webserver should host the freeboard folder, not the direct path to index.

--- web/board.go
+++ web/board.go
@@ -7,7 +7,7 @@ import (
 
 // InitBoard will start the web server and host index.html.
 func InitBoard() {
-	http.Handle("/", http.FileServer(http.Dir("/home/hub/freeboard/index.html")))
+	http.Handle("/", http.FileServer(http.Dir("/home/hub/freeboard")))
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		fmt.Println(err)
