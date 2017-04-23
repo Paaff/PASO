9601d61d9ce7835dfccf4aa549a875b28fd80b5c
Pete <pafischer1992@gmail.com>
2017-04-23 13:43:05 +0200

    Addeed web package and initialization function for hosting freeboard.

---
+++ web/board.go
@@ -0,0 +1,9 @@
+package web
+
+import "net/http"
+
+// InitBoard will start the web server and host index.html.
+func InitBoard() {
+	http.Handle("/", http.FileServer(http.Dir("/home/hub/freeboard/index.html")))
+	http.ListenAndServe(":3000", nil)
+}
