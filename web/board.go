5688cda10d96985296bfe6f4075505735aec38dc
Pete <pafischer1992@gmail.com>
2017-04-23 13:58:06 +0200

    If initboard fails, it should print the error

--- web/board.go
+++ web/board.go
@@ -1,9 +1,15 @@
 package web
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // InitBoard will start the web server and host index.html.
 func InitBoard() {
 	http.Handle("/", http.FileServer(http.Dir("/home/hub/freeboard/index.html")))
-	http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
