e3a01fd241abdc9a227aaf55bea7f12a159fa20c
Pete <pafischer1992@gmail.com>
2017-04-24 21:12:54 +0200

    Added table for the detected clients

--- web/js/infoboard.js
+++ web/js/infoboard.js
@@ -1,6 +1,17 @@
+
+// Bluetooth Client Component
+Vue.component('client-item', {
+  props: ['client'],
+  template: '<tr><td>{{ client.name }}</td><td>{{ client.btaddress }}</td><td>{{ client.class }}</td><td>{{ client.timestamp }}</td></tr>'
+});
+
+// Main Vue
 new Vue({
   el: '#app',
   data: {
-    message: 'Hello infoboard.js!'
+    clientList: [
+      { name: 'Mathias', btaddress: '12:5A:3F:3D:15', class: 'Smartphone class', timestamp: '13:37'},
+      { name: 'Peter', btaddress: '3B:2C:5F:7E:12', class: 'Smartphone class', timestamp: '01:37'}
+    ]
   }
 });
