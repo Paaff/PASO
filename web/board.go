c30708e37d575937526e32cdfdcd998288de166e
Pete <pafischer1992@gmail.com>
2017-04-24 17:23:13 +0200

    Decided to leave Freeboard and create own webpage.

--- web/board.go
+++ web/board.go
@@ -5,20 +5,16 @@ import (
 	"net/http"
 )
 
-// Counter test
-var Counter int
-
 // InitBoard will start the web server and host index.html.
 func InitBoard() {
-	Counter = 0
-	freeboard := "/home/hub/freeboard"
+	freeboard := "/web"
 	http.Handle("/", http.FileServer(http.Dir(freeboard)))
 
 	mux := http.NewServeMux()
 	http.Handle("/api/", http.StripPrefix("/api", mux))
 	mux.HandleFunc("/data", RetrieveBTData)
 
-	err := http.ListenAndServe("192.168.0.109:3000", nil)
+	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -26,6 +22,5 @@ func InitBoard() {
 
 // RetrieveBTData will provide bluetooth data gathered from the system.
 func RetrieveBTData(w http.ResponseWriter, r *http.Request) {
-	Counter++
-	fmt.Fprintf(w, "This is the %v time the endpoint has been visited", Counter)
+
 }
