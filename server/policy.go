0820f46588b3de402b3a87815fea53c1990987de
Pete <pafischer1992@gmail.com>
2017-04-26 18:18:48 +0200

    Remember to ini map

--- server/policy.go
+++ server/policy.go
@@ -4,5 +4,6 @@ import "github.com/paaff/PASO/store"
 
 // InitPolicy - Initializes the policy loading.
 func initPolicy() {
+	store.ValidClients = make(map[string]string)
 	store.ValidClients["24:DA:9B:BB:EE:2B"] = "Peter Fischer"
 }
